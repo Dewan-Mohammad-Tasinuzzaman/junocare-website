c6078042bb3befcfa8188074b849be2834f815cf
Dewan Mohammad Tasinuzzaman <dewanmohammad.tasinuzzaman@ontariotechu.net>
2024-08-15 15:05:46 -0400

    Intentional Site Kill - Layout.tsx

---
+++ app/[lng]/noromby-guide/layout.tsx
@@ -0,0 +1,67 @@
+import type { Metadata } from "next";
+import { Lato } from "next/font/google";
+import "../../styles/main.scss";
+import NavBar from "./NavBar";
+import { Providers } from './Providers';
+import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
+import SmoothScroll from "@/components/SmoothScroll";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
+import Footer from "./Footer";
+import { dir } from 'i18next'
+import { languages } from '../i18n/settings'
+
+
+
+// LANGUAGE-CHANGE RELATED
+export async function generateStaticParams() {
+  return languages.map((lng) => ({ lng }))
+}
+
+
+
+// FONTS
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+  variable: '--font-lato',
+});
+
+
+
+// META-TAGS
+export const metadata: Metadata = {
+  title: "JunoCare",
+  description: "Advancing The Healthcare of Canadians",
+};
+
+
+
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+  params: {
+    lng: string;
+  };
+};
+
+// RootLayout component
+export default function RootLayout({ children, params }: RootLayoutProps) {
+  const { lng } = params;
+
+  return (
+    <html lang={lng} dir={dir(lng)}>
+      <body className={lato.className}>
+        <NavBar params={params} />
+        <Providers>
+          <SmoothScroll>
+            {children}
+          </SmoothScroll>
+        </Providers>
+        <Footer params={{ lng }}/>
+        <SpeedInsights />
+        <Analytics />
+      </body>
+    </html>
+  );
+}
